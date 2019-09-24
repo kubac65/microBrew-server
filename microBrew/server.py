1cd36773fa45c4c61af85870217faa111734e64a
Jakub Potocki <kubac65@gmail.com>
2019-09-24 23:26:37 +0100

    There was some weird issue where last tcp message would not be sent to the contlroller...it's solved now

--- microBrew/server.py
+++ microBrew/server.py
@@ -6,43 +6,35 @@ brew_id = 'brew-1' # Hardcoded for now
 
 class Server(object):
     def __init__(self, temp_logger, decision_module):
-        self.__sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.__listen = False
         self.__temp_logger = temp_logger
         self.__decision_module = decision_module
 
     def start(self):
-        self.__sock.bind(('', listen_port))
-        self.__sock.listen(1)
-        self.__listen = True
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.bind(('', listen_port))
+        sock.listen(1)
 
-        while self.__listen:
+        while True:
             logging.info('Waiting for connections')
-            connection, _ = self.__sock.accept()
-            logging.info('Connection accepted')
+            connection, address = sock.accept()
+            logging.info(f'Connection accepted from: {address}')
 
-            received_msg = []
-            continue_reading = True
-            while continue_reading:
-                data = connection.recv(16)
-                if data:
-                    received_msg.append(data)
-                else:
-                    beer_temp, ambient_temp, heater_state = b''.join(received_msg).decode('utf-8').split(',')
-                    logging.info(f'Beer temp: {beer_temp}   Ambient temp: {ambient_temp}')
+            while True:
+                data = connection.recv(1024)
+                if not data:
+                    break
 
-                    # Decision module will tell us whether the heater needs to be turned on or off.
-                    # But, together with that we'll send the temp ranges to the controller.
-                    # This will ensure that the controller will be able to maintaing the temperature even if the network connection has been proken.
-                    desired_heater_state, min_temp, max_temp = self.__decision_module.get_desired_state(brew_id, float(beer_temp), float(ambient_temp), bool(heater_state))
-                    self.__temp_logger.log(brew_id, beer_temp, ambient_temp, bool(heater_state), desired_heater_state)
-                    msg = f'{1 if desired_heater_state else 0},{min_temp},{max_temp}'
+                beer_temp, ambient_temp, heater_state = data.decode('ascii').split(',')
+                logging.info(f'Beer temp: {beer_temp}')
+                logging.info(f'Ambient temp: {ambient_temp}')
 
-                    # Send response (heating on/off)
-                    connection.send(msg.encode('utf-8'))
-                    logging.debug(f'Response: {msg}')
+                # Decision module will tell us whether the heater needs to be turned on or off.
+                # But, together with that we'll send the temp ranges to the controller.
+                # This will ensure that the controller will be able to maintaing the temperature even if the network connection has been proken.
+                desired_heater_state, min_temp, max_temp = self.__decision_module.get_desired_state(brew_id, float(beer_temp), float(ambient_temp), bool(heater_state))
+                self.__temp_logger.log(brew_id, beer_temp, ambient_temp, bool(heater_state), desired_heater_state)
 
-                    continue_reading = False
+                rsp = f'{1 if desired_heater_state else 0},{min_temp},{max_temp}'.encode('ascii')
+                connection.sendall(rsp)
 
-    def stop(self):
-        self.__listen = False
+            connection.close()
