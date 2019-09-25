44679e8b3e97c955d616e1a8cfd8336fec2e89c2
Jakub Potocki <kubac65@gmail.com>
2019-09-25 23:22:22 +0100

    Added binary protocol handling

--- microBrew/server.py
+++ microBrew/server.py
@@ -1,8 +1,12 @@
 import socket
 import logging
+import struct
 
 listen_port = 52100
-brew_id = 'brew-1' # Hardcoded for now
+brew_id = 'brew-1'  # Hardcoded for now
+
+rcv_msg_size = 16
+
 
 class Server(object):
     def __init__(self, temp_logger, decision_module):
@@ -19,22 +23,40 @@ class Server(object):
             connection, address = sock.accept()
             logging.info(f'Connection accepted from: {address}')
 
-            while True:
-                data = connection.recv(1024)
-                if not data:
-                    break
+            brew_id, beer_temp, ambient_temp, heater_state, cooler_state = Server.__receive_message(connection)
 
-                beer_temp, ambient_temp, heater_state = data.decode('ascii').split(',')
-                logging.info(f'Beer temp: {beer_temp}')
-                logging.info(f'Ambient temp: {ambient_temp}')
+            logging.info(f'Brew id: {brew_id}')
+            logging.info(f'Beer temp: {beer_temp}')
+            logging.info(f'Ambient temp: {ambient_temp}')
+            logging.info(f'Heater state: {heater_state}')
+            logging.info(f'Cooler state: {cooler_state}')
 
-                # Decision module will tell us whether the heater needs to be turned on or off.
-                # But, together with that we'll send the temp ranges to the controller.
-                # This will ensure that the controller will be able to maintaing the temperature even if the network connection has been proken.
-                desired_heater_state, min_temp, max_temp = self.__decision_module.get_desired_state(brew_id, float(beer_temp), float(ambient_temp), bool(heater_state))
-                self.__temp_logger.log(brew_id, beer_temp, ambient_temp, bool(heater_state), desired_heater_state)
+            # Decision module will tell us whether the heater needs to be turned on or off.
+            # But, together with that we'll send the temp ranges to the controller.
+            # This will ensure that the controller will be able to maintaing the temperature even if the network connection has been proken.
+            desired_heater_state, min_temp, max_temp = self.__decision_module.get_desired_state(brew_id, float(beer_temp), float(ambient_temp), bool(heater_state))
+            self.__temp_logger.log(brew_id, beer_temp, ambient_temp, bool(heater_state), desired_heater_state)
 
-                rsp = f'{1 if desired_heater_state else 0},{min_temp},{max_temp}'.encode('ascii')
-                connection.sendall(rsp)
-
+            Server.__send_message(connection, brew_id, desired_heater_state, False, min_temp, max_temp)
             connection.close()
+
+    @staticmethod
+    def __receive_message(sock):
+        received_bytes = 0
+        chunks = []
+        while received_bytes < rcv_msg_size:
+            chunk = sock.recv(rcv_msg_size - received_bytes)
+            chunks.append(chunk)
+            received_bytes = received_bytes + len(chunk)
+
+        # Message comes in in the following binary format and the byte order is little-endian
+        # |--brew id--|--beer temp--|--ambient temp--|--heater state--|--cooler state--|
+        # |--4 bytes--|--4 bytes----|--4 bytes-------|--2 bytes-------|--2 bytes-------|
+        # |--integer--|--float------|--float---------|--bool----------|--bool----------|
+        brew_id, beer_temp, ambient_temp, heater_state, cooler_state = struct.unpack('<IffHH', b''.join(chunks))
+        return (str(brew_id), float(beer_temp), float(ambient_temp), bool(heater_state), bool(cooler_state))
+
+    @staticmethod
+    def __send_message(sock, brew_id, heater_state, cooler_state, min_temp, max_temp):
+        msg = struct.pack('<IHHff', int(brew_id), heater_state, cooler_state, min_temp, max_temp)
+        sock.sendall(msg)
