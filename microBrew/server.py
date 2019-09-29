b8a0e8a3a1ab0f8b6db57a089d0615849d945ecf
Jakub Potocki <kubac65@gmail.com>
2019-09-29 23:13:59 +0100

    Added response fomat comment

--- microBrew/server.py
+++ microBrew/server.py
@@ -49,5 +49,9 @@ class Server(object):
 
     @staticmethod
     def __send_message(sock, brew_id, heater_state, cooler_state, min_temp, max_temp):
+        # Message comes in in the following binary format and the byte order is little-endian
+        # |--brew id--|--heater state--|--cooler state--|--min temp--|--max temp--|
+        # |--4 bytes--|--2 bytes-------|--2 bytes-------|--4 bytes---|--4 bytes---|
+        # |--integer--|--bool----------|--bool----------|--float-----|--float-----|
         msg = struct.pack('<IHHff', int(brew_id), heater_state, cooler_state, min_temp, max_temp)
         sock.sendall(msg)
