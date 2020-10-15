76b9ec7d25176571c621c527054a89eed25a42d3
Jakub Potocki <kubac65@gmail.com>
2020-10-15 20:06:19 +0100

    Updating log format

--- src/program.py
+++ src/program.py
@@ -5,7 +5,7 @@ from microBrew import server_factory
 
 
 def main():
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.DEBUG, format="%(asctime)-15s %(clientip)s %(user)-8s %(message)s")
     srv = server_factory.create_server()
     srv.start()
 
